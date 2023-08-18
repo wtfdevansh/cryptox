1b326194ad5613abf184ee0d29cd71f9711a93eb
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-08-18 20:44:06 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://cryptox-wijr.vercel.app",
   })
 );
 app.use(express.static(__dirname + '/public/'))
