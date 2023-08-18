b3ec785b1bb09dc4e84be58f5a82ee705fe0624f
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-08-18 23:55:36 +0530

    Update headerdatabase.js

--- backend/database.js
+++ backend/database.js
@@ -17,6 +17,13 @@ app.use(
     
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(express.static(__dirname + '/public/'))
 
 
