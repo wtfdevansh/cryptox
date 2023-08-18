7500b08fe60197cfa8a52a3867af97b2d7447507
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-08-18 23:42:27 +0530

    Update lasttdatabase.js

--- backend/database.js
+++ backend/database.js
@@ -14,7 +14,7 @@ app.use(
   cors({
   "origin": "*",
   methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    credentials: true
+    
   })
 );
 app.use(express.static(__dirname + '/public/'))
