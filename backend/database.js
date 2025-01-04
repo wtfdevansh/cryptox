0227e3d4ec56bbe3a76b067f51bdc2cd4ba1cf3e
wtfdevansh <mrdivu7@gmail.com>
2025-01-04 10:02:10 +0530

    updateverce

--- backend/database.js
+++ backend/database.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
+  next();
 })
 app.use(express.static(__dirname + '/public/'))
 
