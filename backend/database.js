a1ea90b990909b095f560524fcd2989f9e832d61
wtfdevansh <mrdivu7@gmail.com>
2025-01-04 17:08:25 +0530

    cors

--- backend/database.js
+++ backend/database.js
@@ -10,7 +10,9 @@ mongoose.connect(DB);
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ 
+  origin: 'http://localhost:3001/' 
+}));
 
 
 
